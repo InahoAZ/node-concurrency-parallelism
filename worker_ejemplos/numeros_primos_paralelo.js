63553f768b9f309930ab4c4c1cec52ba86922767
Santiago Villar <santiago.villar@unam.edu.ar>
2023-03-15 11:23:32 -0300

    Añade mas comentarios al codigo

--- worker_ejemplos/numeros_primos_paralelo.js
+++ worker_ejemplos/numeros_primos_paralelo.js
@@ -30,14 +30,17 @@ function generatePrimes(start, range) {
 
 //Generamos n numeros primos paralelamente
 if (isMainThread) {
-const max = 1e7;
-const threadCount = +process.argv[2] || 2;
-const threads = new Set();
-console.log(`Running with ${threadCount} threads...`);
+    const max = 1e7;
+    const threadCount = +process.argv[2] || 2;
+    const threads = new Set();
+    console.log(`Running with ${threadCount} threads...`);
 
-//Obtenemos en cuanto particionar el conjunto segun la cantidad de threads a crear.
-const range = Math.ceil((max - min) / threadCount);
-let start = min;
+    //Realizamos una Descomposición de Dominio segun la cantidad de threads a crear.
+    const range = Math.ceil((max - min) / threadCount);
+    let start = min;
+
+    /** Creo un worker por cada porcion del dominio pasandole por el constructor 
+    informacion del rango a calcular. */
     for (let i = 0; i < threadCount - 1; i++) {
         const myStart = start;
         threads.add(
@@ -50,22 +53,30 @@ let start = min;
             workerData: { start, range: range + ((max - min + 1) % threadCount) },
         })
     );
+
+    //Creo manejadores de eventos por cada worker. Quedan escuchando en segundo plano.
+    //Se ejecutan una vez el worker dispare uno de esos eventos
     for (let worker of threads) {
         worker.on("error", (err) => {
             throw err;
         });
+        //Cuando un worker finaliza su ejecucion, se ejecuta esto.
         worker.on("exit", () => {
             threads.delete(worker);
             console.log(`Thread exiting, ${threads.size} running...`);
             if (threads.size === 0) {
-            console.log(primes.join("\n"));
+                console.log(primes.join("\n"));
             }
         });
+        //Cuando el hilo principal recibe un mensaje con postMessage se ejecuta esto.
         worker.on("message", (msg) => {
             primes = primes.concat(msg);
         });
     }
 } else {
+    //A traves de la informacion recibida por workerData, genero los numeros primos en el rango provisto.
     generatePrimes(workerData.start, workerData.range);
+
+    //Mando al hilo principal los numeros primos en el rango provisto al worker por mensaje.
     parentPort.postMessage(primes);
 }
